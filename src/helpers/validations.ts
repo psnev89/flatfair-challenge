75e34bc0cfc6d2bf790cfe257c284b612ec14db7
Pedro Santana Neves <pneves@bladeinsight.com>
2022-08-12 19:40:09 +0100

    feat: add tests for validations

--- src/helpers/validations.ts
+++ src/helpers/validations.ts
@@ -2,11 +2,17 @@ import { RentPeriod, RentPeriodEnum } from "../types/rent.types";
 import { TaskResult, Pound } from "../types/shared.types";
 import { getRentRangeByRentPeriod, isNumberBetween } from "./utils";
 
+/**
+ * Method to validate rent amount.
+ * @param amount - Amount to be validated
+ * @param rentPeriod - Rent period. Can be any of {RentPeriod} type.
+ * @returns {TaskResult<boolean>} Returns tupple with ["error message if applicable", successBoolean]
+ */
 export function validateRentAmount(amount: Pound, rentPeriod: RentPeriod = RentPeriodEnum.Week): TaskResult<boolean> {
   // validation amount input min & max not needed as range will comply (BR1)
 
   const [minRange, maxRange]: [Pound, Pound] = getRentRangeByRentPeriod(rentPeriod); // get rent range constraint
   if (isNumberBetween(amount, minRange, maxRange)) return [null, true]; // validate (BR3.1 & BR3.2)
 
-  return [`Error: Rent amount (${amount}£) not in range of [${minRange}£ ~ ${maxRange}£]`, false];
+  return [`Error: Rent amount (£${amount}) not in range of [£${minRange} ~ £${maxRange}]`, false];
 }
