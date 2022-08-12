3624ed1a54251df01e032ca65feb38b4b28fa81d
Pedro Santana Neves <pneves@bladeinsight.com>
2022-08-12 19:01:34 +0100

    chore: add and refine configs

--- tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import {init} from "../src/index.js";
-
-describe("init dummy test", () => {
-  test("init function should return boolean", () => {
-    expect(init("test")).toBeTruthy();
-  });
-});
