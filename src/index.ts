5aff5eebc430a998d6bcbea479c63972d545b230
Pedro Santana Neves <pneves@bladeinsight.com>
2022-08-12 15:52:52 +0100

    feat: update main function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-export function init(str: string): boolean {
-  return !!str;
-}
+import { MINIMUM_MEMBERSHIP_FEE } from "./helpers/constants";
+import { calcVat } from "./helpers/utils";
+import { validateRentAmount } from "./helpers/validations";
+import { OrganisationUnit } from "./types/organisation.types";
+import { RentPeriod } from "./types/rent.types";
+import { Pound } from "./types/shared.types";
 
-let x = init("test");
-console.log(x);
+/**
+ * Function to calculate membership fee.
+ * @param rentAmount - Rent amount. from 1 to Number.MAX_SAFE_INTEGER
+ * @param rentPeriod - Rent period. Can be one of: [week, month]
+ * @param organisationUnit  - Branch
+ * @returns 
+ */
+export function calculateMembershipFee(rentAmount: Pound, rentPeriod: RentPeriod, organisationUnit: OrganisationUnit): Pound {
+  // Membership fee calculation rules (in order, from least to most important)
+  //// TODO: Recursive search for organisation config object property fixed_membership_fee
+
+  const [error] = validateRentAmount(rentAmount, rentPeriod);   // Validate rentAmount by rentPeriod
+  if (error) throw new RangeError(error); // Throw if invalid (BR3)
+
+  //// TODO: Membership fee is equal to one week of rent + VAT
+
+  //// Minimum membership fee is £120 + VAT
+  return MINIMUM_MEMBERSHIP_FEE + calcVat(MINIMUM_MEMBERSHIP_FEE);
+}
